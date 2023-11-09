9f511f29d1f526a6d8fe2cd7b333019ea9c24924
Pointless <150302154+PointlessCircle@users.noreply.github.com>
2023-11-09 13:01:45 -0500

    Added the search.js file
    This file controls the actual search functions

---
+++ search.js
@@ -0,0 +1,29 @@
+let catList = []; //declare 'catList'
+let outString = ""; //declare 'outString'
+let catPlus = "Category:"
+
+
+
+function ddC(cat) {
+    let index = catList.indexOf(cat); //sets 'index' to the first instance of the param 'cat'
+    if (index == -1) { //if 'cat' is not in array
+        catList.push(cat);  //add 'cat' to the end
+        document.getElementById(cat).innerHTML = "<b>"+cat+"</b>";
+        //bold  
+    } else { //if 'cat' is in array
+        catList.splice(index,1); //remove it from the array
+        document.getElementById(cat).innerHTML = cat;
+    }
+}
+
+function catg(value) {
+    outString += catPlus + value + "<br>"; 
+    //set output to "Catagory:" + the value of the array element + newline
+}
+
+function output() {
+    catList.forEach(catg) //run catg for every array element
+    document.getElementById("output").innerHTML = outString;
+    //output outString into DPL params
+    outString = ""; //empty outString to allow next run
+}
